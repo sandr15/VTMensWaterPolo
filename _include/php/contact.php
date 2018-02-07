2b726ada75a35af304621c3927fcafec3e011bf9
Tom Evans <tsevans@vt.edu>
2018-02-07 13:17:11 -0500

    Fixed email and added Jarid info

--- _include/php/contact.php
+++ _include/php/contact.php
@@ -8,21 +8,21 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$admin_email = 'your@yourdomain.com'; // Your Email
+$admin_email = 'briand15@vt.edu'; // Your Email
 $message_min_length = 5; // Min Message Length
 
 
 class Contact_Form{
 	function __construct($details, $email_admin, $message_min_length){
-		
+
 		$this->name = stripslashes($details['name']);
 		$this->email = trim($details['email']);
-		$this->subject = 'Contact from Your Website'; // Subject 
+		$this->subject = 'Contact from Your Website'; // Subject
 		$this->message = stripslashes($details['message']);
-	
+
 		$this->email_admin = $email_admin;
 		$this->message_min_length = $message_min_length;
-		
+
 		$this->response_status = 1;
 		$this->response_html = '';
 	}
@@ -30,13 +30,13 @@ class Contact_Form{
 
 	private function validateEmail(){
 		$regex = '/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i';
-	
-		if($this->email == '') { 
+
+		if($this->email == '') {
 			return false;
 		} else {
 			$string = preg_replace($regex, '', $this->email);
 		}
-	
+
 		return empty($string) ? true : false;
 	}
 
@@ -55,14 +55,14 @@ class Contact_Form{
 			$this->response_html .= '<p>Please enter an e-mail address</p>';
 			$this->response_status = 0;
 		}
-		
+
 		// Check valid email
 		if($this->email && !$this->validateEmail())
 		{
 			$this->response_html .= '<p>Please enter a valid e-mail address</p>';
 			$this->response_status = 0;
 		}
-		
+
 		// Check message length
 		if(!$this->message || strlen($this->message) < $this->message_min_length)
 		{
@@ -77,7 +77,7 @@ class Contact_Form{
 			 "From: ".$this->name." <".$this->email.">\r\n"
 			."Reply-To: ".$this->email."\r\n"
 		."X-Mailer: PHP/" . phpversion());
-	
+
 		if($mail)
 		{
 			$this->response_status = 1;
@@ -94,9 +94,9 @@ class Contact_Form{
 		}
 
 		$response = array();
-		$response['status'] = $this->response_status;	
+		$response['status'] = $this->response_status;
 		$response['html'] = $this->response_html;
-		
+
 		echo json_encode($response);
 	}
 }
